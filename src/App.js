46a360c6dd0a4f5219678e61ad7a7718ac6afef1
WLado <wirejodisfj@df.com>
2019-05-20 18:55:51 +0200

    Repair mistakes in filrs app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-
+import {robots} from './robots';
 const App = () => {
     return (
         <div>
