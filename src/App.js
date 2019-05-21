c6e7c863e5954079ba812a4f0e6dd1a54cea504f
WLado <wirejodisfj@df.com>
2019-05-21 22:24:37 +0200

    created scroll for search box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -30,7 +31,9 @@ class App extends React.Component {
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
         );
     }
