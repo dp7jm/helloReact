18b1ad0b4830705c323e533dd88cc3d9de304051
WLado <wirejodisfj@df.com>
2019-05-21 22:04:34 +0200

    added componetDidMount and fetched users froms jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -9,25 +9,21 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: []
+            robots: [],
             searchfield: ''
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=> {
-            return response.jsone();
-        })
-        .then(users => {
-        this.setState({robots: users})
-        });
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
     onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value})                 
+        this.setState({ searchfield: event.target.value })
 
     }
     render() {
-        const filteredRobots = this.state.robots.filter(robots =>{
+        const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
         return (
